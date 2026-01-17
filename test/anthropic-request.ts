d09b03207af778bded5b498260bc50e398c9e992
zwvx <165436409+zwvx@users.noreply.github.com>
2026-01-16 22:00:35 -0500

    anth test

--- test/anthropic-request.ts
+++ test/anthropic-request.ts
@@ -2,13 +2,17 @@ import Anthropic from "@anthropic-ai/sdk"
 
 if (import.meta.main) {
     const url = Bun.argv[2]!
+    const model = Bun.argv[3]!
+
     const anthropic = new Anthropic({
         baseURL: url,
         apiKey: 'sk-test'
     })
 
-    await anthropic.messages.create({
-        model: 'test-model',
+    // console.log(`models:`, (await anthropic.models.list()).data)
+
+    await anthropic.messages.stream({
+        model: model,
         messages: [
             {
                 role: 'user',
@@ -16,5 +20,5 @@ if (import.meta.main) {
             }
         ],
         max_tokens: 100
-    })
+    }).on('text', (textDelta) => process.stdout.write(textDelta)).on('end', () => process.stdout.write('\n'))
 }
